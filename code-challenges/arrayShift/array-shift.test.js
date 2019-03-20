aa8adc343c5e56f5a79b0ce8af10f9e85d998496
Wei9023 <vv880923@gmail.com>
2019-03-20 01:07:07 -0700

    array-shift

---
+++ code-challenges/arrayShift/array-shift.test.js
@@ -0,0 +1,18 @@
+const arrShift = require('./array-shift.js');
+
+describe('Array-shift', () =>{
+    const arrOdd = [2,4,6,8];
+    const arrEven = [4,8,15,23,42];
+    const valOne = 5;
+    const valTwo = 16;
+
+    it('To be insert into middle index', () => {
+        let expectedOutput = arrShift.insertShiftArray(arrOdd, valOne);
+        expect(expectedOutput).toEqual([2,4,5,6,8]);
+    })
+
+    it('To be insert into middle index', () => {
+        let expectedOutput = arrShift.insertShiftArray(arrEven, valTwo);
+        expect(expectedOutput).toEqual([4,8,15,16,23,42]);
+    })
+})
